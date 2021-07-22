3780eb94a5607bde220515d6bd99eefa2bef1969
Dev Wadhwa <devwadhwa01@gmail.com>
2021-07-22 23:46:32 +0530

    Shortened JS code

--- index.js
+++ index.js
@@ -8,57 +8,14 @@ function randomDice()
 var no1=randomDice();
 var no2=randomDice();
 
+var src1="images/dice"+no1+".gif";
+var src2="images/dice"+no2+".gif";
+
 // Dice 1
-if(no1===1)
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice1.gif");
-}
-else if(no1===2)
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice2.gif");
-}
-else if(no1===3)
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice3.gif");
-}
-else if(no1===4)
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice4.gif");
-}
-else if(no1===5)
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice5.gif");
-}
-else
-{
-  document.querySelector(".firstDice").setAttribute("src","images/dice6.gif");
-}
+document.querySelector(".firstDice").setAttribute("src",src1);
 
 // Dice 2
-if(no2===1)
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice1.gif");
-}
-else if(no2===2)
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice2.gif");
-}
-else if(no2===3)
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice3.gif");
-}
-else if(no2===4)
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice4.gif");
-}
-else if(no2===5)
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice5.gif");
-}
-else
-{
-  document.querySelector(".secondDice").setAttribute("src","images/dice6.gif");
-}
+document.querySelector(".secondDice").setAttribute("src",src2);
 
 //Timeout
 setTimeout(function(){
